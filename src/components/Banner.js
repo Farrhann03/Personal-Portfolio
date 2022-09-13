3d80fc33a6b07d60ad0939a1d5a8e6009c0acc05
Farrhann03 <98152749+Farrhann03@users.noreply.github.com>
2022-09-13 21:55:23 +0800

    change cv link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,7 +17,7 @@ export const Banner = () => {
                         <span className="tagline"> Welcome to my Portfolio</span>
                         <h1>{`Hello, I'm Farhan, a software developer. `}</h1>
                         <p>I love tech and creating. I'm a lifelong learner and I like taking photos.</p>
-                        <button onClick={() => openInNewTab('https://drive.google.com/file/d/1P143LMr14B39qd2NtJ5fgJbsnhbTn62n/view?usp=sharing')}> View my CV <ArrowRightCircle size={25}/></button>
+                        <button onClick={() => openInNewTab('https://drive.google.com/file/d/1NU6W2VPnJjM4BR7ScsR40bWQITH4GTiN/view?usp=sharing')}> View my CV <ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={ProfileImage} alt="Profile Img" className="profile-image" />
