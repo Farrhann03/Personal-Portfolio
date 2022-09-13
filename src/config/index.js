632617eeee8cf6d89dc699f1b819cbcd0512dffe
Farrhann03 <98152749+Farrhann03@users.noreply.github.com>
2022-09-13 22:37:52 +0800

    deploy test

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+const env = proces.env.REACT_APP_ENV;
+
+let config = {
+    baseURL: "https://farrhann03.github.io/Personal-Portfolio/"
+}
+
+switch(env.toUpperCase()){
+    case "STAGE":
+        config.baseURL = "https://stage.farrhann03.github.io/Personal-Portfolio/"
+        break;
+    case "PRODUCTION":{
+        config.baseURL = "https://farrhann03.github.io/Personal-Portfolio/"
+        break;
+    }
+}
+
+export default config;
